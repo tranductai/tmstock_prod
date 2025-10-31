09b254a593b1c412697b464a19e5a272ecf46437
Tai Tran <ttran267@dxc.com>
2025-10-31 13:46:43 +0700

    change url environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://tmstock-nodejs.onrender.com/api',
+  apiUrl: 'https://tmstock-nodejs.onrender.com',
   ResetPass_url: 'https://tmstock.netlify.app'
 };
