2c0cef5a7ae92b09fc54a9139470e222d77d0cd8
Tai Tran <ttran267@dxc.com>
2025-10-31 13:41:54 +0700

    change url for resetpass

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://tmstock-nodejs.onrender.com/api'
+  apiUrl: 'https://tmstock-nodejs.onrender.com/api',
+  ResetPass_url: 'https://tmstock.netlify.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  ResetPass_url: 'http://localhost:4005'
+  apiUrl: 'https://tmstock-nodejs.onrender.com/api',
+  ResetPass_url: 'https://tmstock.netlify.app'
 };
